01bef2f9bbd01d5e33fc789a2eab0e26c0ae35be
papi zen <zentot015@gmail.com>
2026-09-21 17:28:42 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'cyber': ['"Share Tech Mono"', 'monospace'],
+      },
+      colors: {
+        'dark-bg': '#0a0a0a',
+        'neon-red': '#ff003c',
+      }
+    },
+  },
+  plugins: [],
+}
