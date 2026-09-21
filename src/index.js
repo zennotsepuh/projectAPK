657b6e3bfeeb989dccf8094ae54c79c162d9cbe7
papi zen <zentot015@gmail.com>
2026-09-21 17:29:25 +0700

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+@import url('https://fonts.googleapis.com/css2?family=Share+Tech+Mono&display=swap');
+
+body {
+  background-color: #0a0a0a;
+  color: white;
+  font-family: 'Share Tech Mono', monospace;
+}
